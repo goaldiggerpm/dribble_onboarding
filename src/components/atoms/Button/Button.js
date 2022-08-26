c350b7f1619f7c0a48fee4ef0918a7d8cddd2df3
prince37 <prince@binaryveda.com>
2022-08-26 17:52:14 +0530

    [ architecture || button ] - Dev - Prince

---
+++ src/components/atoms/Button/Button.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Button = ({ text }) => {
+
+    return (
+        <button className="main-button" >
+            {text}
+        </button>
+    );
+}
+
+export default Button;
---
+++ src/pages/onboarding/Index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MyButton from '../../components/atoms/Button/Button.js'
+
+const Index = () => {
+
+    return (
+        <div>
+            <MyButton text={"Hii Pm"} />
+        </div>
+    )
+}
+
+export default Index
